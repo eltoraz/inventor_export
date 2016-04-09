3e4b5f3c728ed01ac7c0544a865689e65b9941c2
Bill Jameson <williamjameson@alum.rpi.edu>
2016-04-09 00:25:50 -0400

    Start parsing Excel doc

--- part_format.py
+++ part_format.py
@@ -5,5 +5,17 @@ Third-party dependencies: xlrd (https://pypi.python.org/pypi/xlrd)
 import xlrd
 
 # TODO:
-# - xlrd library from pypi
 # - open XLS, grab relevant data, construct CSV
+# - check that the required fields have been filled
+# - validate input
+# - log activity
+
+filename = 'Part_Level.xls'
+worksheet = xlrd.open_workbook(filename).sheet_by_index(0)
+title_row = worksheet.row(0)
+data_row = worksheet.row(1)
+
+# every empty cell should be the same object as xlrd.empty_cell,
+# but comparisons still return False
+data = [field for field in zip(title_row, data_row)
+        if field[1].value != xlrd.empty_cell.value]
