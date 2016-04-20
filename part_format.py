3cc2f74d01a6f55fe2f75e1d69d95dcbc75cd229
Bill Jameson <williamjameson@alum.rpi.edu>
2016-04-19 22:06:54 -0400

    Extend ilogic script to cover all needed fields
    If the GoExcel object works with CSV files, the python script won't
    even be needed

--- part_format.py
+++ part_format.py
@@ -15,7 +15,7 @@ worksheet = xlrd.open_workbook(filename).sheet_by_index(0)
 title_row = worksheet.row(0)
 data_row = worksheet.row(1)
 
-# every empty cell should be the same object as xlrd.empty_cell,
-# but comparisons still return False
-data = [field for field in zip(title_row, data_row)
+# Note: the documentation states that xlrd.empty_cell is a singleton,
+# but this is false: https://github.com/python-excel/xlrd/issues/162
+data = [(field[0].value, field[1].value) for field in zip(title_row, data_row)
         if field[1].value != xlrd.empty_cell.value]
