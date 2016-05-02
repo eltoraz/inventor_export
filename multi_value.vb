58c330bd84820989308d5af37dc241b8adb9f9a8
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-02 17:12:04 -0400

    Hackish support for boolean
    If only Autodesk had a comprehensive API reference online

--- multi_value.vb
+++ multi_value.vb
@@ -1,15 +1,45 @@
 Sub Main()
     'change Custom iProperty Name to desired Custom iProperty Name
-    Dim propertyName1 As String = "Part Type"
+    Dim propertyName1 As String = "PartType"
+    Dim propertyName2 As String = "Group"
+    Dim propertyName3 As String = "ClassID"
+    Dim propertyName4 As String = "UsePartRev"
 
-    createParam("PartType")
-    createParam("Group")
-    createParam("ClassID")
+    createParam(propertyName1)
+    createParam(propertyName2)
+    createParam(propertyName3)
+    createParamBool(propertyName4)
 
-    MultiValue.SetList("PartType", "M", "P")
-    MultiValue.SetList("Group", "FSC Assemblies", "FSC Componenets", "NCA Assemblies", "NCA Components", "Purchases")
-    MultiValue.SetList("ClassID", "Assemblies we sell", "Box Materials", "Components we sell", "Finish Materials", "Finished Components for kits", "Finished Components on shelf", "FSC Lumber", "Lumber", "Other Materials", "Tooling")
+    MultiValue.SetList(propertyName1, "M", "P")
+    MultiValue.SetList(propertyName2, "FSC Assemblies", "FSC Componenets", "NCA Assemblies", "NCA Components", "Purchases")
+    MultiValue.SetList(propertyName3, "Assemblies we sell", "Box Materials", "Components we sell", "Finish Materials", "Finished Components for kits", "Finished Components on shelf", "FSC Lumber", "Lumber", "Other Materials", "Tooling")
 
+    'addProp(propertyName1)
+    'addProp(propertyName2)
+    'addProp(propertyName3)
+End Sub
+
+Sub createParam(ByVal n As String)
+    dim oDoc As Document
+
+    oDoc = ThisApplication.ActiveDocument
+    Dim oParams As UserParameters = oDoc.Parameters.UserParameters
+
+    Dim TestParam As UserParameter = oParams.AddByValue(n, "", UnitsTypeEnum.kTextUnits)
+    TestParam.ExposedAsProperty = True
+End Sub
+
+Sub createParamBool(ByVal n As String)
+    dim oDoc As Document
+
+    oDoc = ThisApplication.ActiveDocument
+    Dim oParams As UserParameters = oDoc.Parameters.UserParameters
+
+    Dim TestParam As UserParameter = oParams.AddByValue(n, True, UnitsTypeEnum.kBooleanUnits)
+    TestParam.ExposedAsProperty = True
+End Sub
+
+Sub addProp(ByVal n As String)
     'define custom property collection
     oCustomPropertySet = ThisDoc.Document.PropertySets.Item("Inventor User Defined Properties")
 
@@ -23,16 +53,7 @@ Sub Main()
 
     'set custom property value; Change Custom iProperty Name to desired Custom iProperty Name;
     'change UniqueFxName to the name of your User Defined Parameter
-    iProperties.Value("Custom", "Part Type") = UniqueFxName
+    iProperties.Value("Custom", "PartType") = UniqueFxName
     'processes update when rule is run so save doesn't have to occur to see change
     iLogicVb.UpdateWhenDone = True
-End Sub
-
-Sub createParam(ByVal n As String)
-    dim oDoc As Document
-
-    oDoc = ThisApplication.ActiveDocument
-    Dim oParams As UserParameters = oDoc.Parameters.UserParameters
-
-    Dim TestParam As UserParameter = oParams.AddByValue(n, "", UnitsTypeEnum.kTextUnits)
 End Sub
