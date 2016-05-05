a6165e2ba4df5fc96e68f98123b2a6912c2667ac
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-05 16:32:34 -0400

    Switch to a dict when creating parameters
    Easier to use a collection and iterate over it, esp. as more fields are
    added

--- multi_value.vb
+++ multi_value.vb
@@ -1,25 +1,27 @@
 'create parameters with a restricted set of accepted values for import by
 'Epicor DMT (the actual IDs the tool needs are set in set_props.vb)
 Sub Main()
-    Dim propertyName1 As String = "PartType"
-    Dim propertyName2 As String = "ProdCode"
-    Dim propertyName3 As String = "ClassID"
-    Dim propertyName4 As String = "UsePartRev"
+    Dim params As New Dictionary(Of String, UnitsTypeEnum)
+    params.Add("PartType", UnitsTypeEnum.kTextUnits)
+    params.Add("ProdCode", UnitsTypeEnum.kTextUnits)
+    params.Add("ClassID", UnitsTypeEnum.kTextUnits)
+    params.Add("UsePartRev", UnitsTypeEnum.kBooleanUnits)
+    params.Add("MfgComment", UnitsTypeEnum.kTextUnits)
+    params.Add("PurComment", UnitsTypeEnum.kTextUnits)
+    params.Add("TrackSerialNum", UnitsTypeEnum.kBooleanUnits)
 
-    createParam(propertyName1, UnitsTypeEnum.kTextUnits)
-    createParam(propertyName2, UnitsTypeEnum.kTextUnits)
-    createParam(propertyName3, UnitsTypeEnum.kTextUnits)
-    createParam(propertyName4, UnitsTypeEnum.kBooleanUnits)
+    For Each kvp As KeyValuePair(Of String, UnitsTypeEnum) in params
+        createParam(kvp.Key, kvp.Value)
+    Next
 
-    MultiValue.SetList(propertyName1, "M", "P")
-    MultiValue.SetList(propertyName2, "FSC Assemblies", "FSC Components", "NCA Assemblies", "NCA Components", "Purchases")
-    MultiValue.SetList(propertyName3, "Assemblies we sell", "Box Materials", "Components we sell", "Finished Components for kits", "Finish Materials", "FSC Lumber", "Finished Components on shelf", "IT Supplies", "Lumber", "Office Supplies", "Other Materials", "Tooling")
+    MultiValue.SetList("PartType", "M", "P")
+    MultiValue.SetList("ProdCode", "FSC Assemblies", "FSC Components", "NCA Assemblies", "NCA Components", "Purchases")
+    MultiValue.SetList("ClassID", "Assemblies we sell", "Box Materials", "Components we sell", "Finished Components for kits", "Finish Materials", "FSC Lumber", "Finished Components on shelf", "IT Supplies", "Lumber", "Office Supplies", "Other Materials", "Tooling")
 End Sub
 
 Sub createParam(ByVal n As String, ByVal paramType As UnitsTypeEnum)
-    dim invDoc As Document
+    dim invDoc As Document = ThisApplication.ActiveDocument
 
-    invDoc = ThisApplication.ActiveDocument
     Dim invParams As UserParameters = invDoc.Parameters.UserParameters
 
     Dim defaultValue
@@ -30,5 +32,6 @@ Sub createParam(ByVal n As String, ByVal paramType As UnitsTypeEnum)
     End If
 
     Dim TestParam As UserParameter = invParams.AddByValue(n, defaultValue, paramType)
-    'TestParam.ExposedAsProperty = True
+
+    invDoc.Update
 End Sub
