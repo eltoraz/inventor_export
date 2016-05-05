7582a8d7de9d92c0917e1ff2c5a605c693e16a51
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-05 11:43:45 -0400

    Make createParam general for strings/bool
    Didn't have time to find/dive into the documentation before, but this
    time I found the data type for the k*Units, so I could merge the 2
    subroutines. Also moving addProp to a different script since it will be
    used much later in the process.

--- multi_value.vb
+++ multi_value.vb
@@ -1,59 +1,34 @@
+'create parameters with a restricted set of accepted values for import by
+'Epicor DMT (the actual IDs the tool needs are set in set_props.vb)
 Sub Main()
-    'change Custom iProperty Name to desired Custom iProperty Name
     Dim propertyName1 As String = "PartType"
     Dim propertyName2 As String = "Group"
     Dim propertyName3 As String = "ClassID"
     Dim propertyName4 As String = "UsePartRev"
 
-    createParam(propertyName1)
-    createParam(propertyName2)
-    createParam(propertyName3)
-    createParamBool(propertyName4)
+    createParam(propertyName1, UnitsTypeEnum.kTextUnits)
+    createParam(propertyName2, UnitsTypeEnum.kTextUnits)
+    createParam(propertyName3, UnitsTypeEnum.kTextUnits)
+    createParam(propertyName4, UnitsTypeEnum.kBooleanUnits)
 
     MultiValue.SetList(propertyName1, "M", "P")
     MultiValue.SetList(propertyName2, "FSC Assemblies", "FSC Componenets", "NCA Assemblies", "NCA Components", "Purchases")
     MultiValue.SetList(propertyName3, "Assemblies we sell", "Box Materials", "Components we sell", "Finish Materials", "Finished Components for kits", "Finished Components on shelf", "FSC Lumber", "Lumber", "Other Materials", "Tooling")
-
-    'addProp(propertyName1)
-    'addProp(propertyName2)
-    'addProp(propertyName3)
 End Sub
 
-Sub createParam(ByVal n As String)
+Sub createParam(ByVal n As String, ByVal paramType As UnitsTypeEnum)
     dim oDoc As Document
 
     oDoc = ThisApplication.ActiveDocument
     Dim oParams As UserParameters = oDoc.Parameters.UserParameters
 
-    Dim TestParam As UserParameter = oParams.AddByValue(n, "", UnitsTypeEnum.kTextUnits)
-    TestParam.ExposedAsProperty = True
-End Sub
+    Dim defaultValue
+    If paramType = UnitsTypeEnum.kTextUnits Then
+        defaultValue = ""
+    ElseIf paramType = UnitsTypeEnum.kBooleanUnits Then
+        defaultValue = True
+    End If
 
-Sub createParamBool(ByVal n As String)
-    dim oDoc As Document
-
-    oDoc = ThisApplication.ActiveDocument
-    Dim oParams As UserParameters = oDoc.Parameters.UserParameters
-
-    Dim TestParam As UserParameter = oParams.AddByValue(n, True, UnitsTypeEnum.kBooleanUnits)
-    TestParam.ExposedAsProperty = True
-End Sub
-
-Sub addProp(ByVal n As String)
-    'define custom property collection
-    oCustomPropertySet = ThisDoc.Document.PropertySets.Item("Inventor User Defined Properties")
-
-    Try
-        'set property value
-        oProp = oCustomPropertySet.Item(propertyName1)
-    Catch
-        ' Assume error means not found so create it
-        oCustomPropertySet.Add("", propertyName1)
-    End Try
-
-    'set custom property value; Change Custom iProperty Name to desired Custom iProperty Name;
-    'change UniqueFxName to the name of your User Defined Parameter
-    iProperties.Value("Custom", "PartType") = UniqueFxName
-    'processes update when rule is run so save doesn't have to occur to see change
-    iLogicVb.UpdateWhenDone = True
+    Dim TestParam As UserParameter = oParams.AddByValue(n, defaultValue, paramType)
+    'TestParam.ExposedAsProperty = True
 End Sub
