4e88eab4c18d44e7c4d344a81caae489c3453af9
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-16 14:43:47 -0400

    Try running remaining rules
    Various forum posts (etc.) imply Inventor may pause execution when
    trying to enter another rule to wait for form closure

--- master_control.vb
+++ master_control.vb
@@ -1,4 +1,5 @@
 'Call the other rules in order
 iLogicVb.RunExternalRule("01multi_value.vb")
-iLogicForm.ShowGlobal("02part_properties")
-MsgBox("Done")
+iLogicForm.ShowGlobal("02part_properties", FormMode.Modal)
+iLogicVb.RunExternalRule("03set_props.vb")
+iLogicVb.RunExternalRule("04part_export.vb")
