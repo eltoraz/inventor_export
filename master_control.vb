55371e2e401da89e0c0400f914e79229c98c10cf
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-16 14:49:15 -0400

    Set master rule to run on iTrigger button

--- master_control.vb
+++ master_control.vb
@@ -1,3 +1,6 @@
+'Set this rule to run when on the iTrigger button
+trigger = iTrigger0
+
 'Call the other rules in order
 iLogicVb.RunExternalRule("01multi_value.vb")
 iLogicForm.ShowGlobal("02part_properties", FormMode.Modal)
