15cd9ae49ee8c08f9a38a242cbbe5a6784f57ede
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-19 14:39:35 -0400

    AddVbFile needs to be at file level
    Enclose the rest of the contents of 04part_export.vb in a subroutine

--- 04part_export.vb
+++ 04part_export.vb
@@ -1,86 +1,88 @@
-﻿AddVBFile "dmt.vb"
+﻿AddVbFile "dmt.vb"
 
-Dim fso, FileName, csv
-Dim Fields, Data
-Dim Description, PartType, UOM, TrackSerialNum
-Dim SNFormat, SNBaseDataType, SNMask, SNMaskExample
+Sub Main()
+    Dim fso, FileName, csv
+    Dim Fields, Data
+    Dim Description, PartType, UOM, TrackSerialNum
+    Dim SNFormat, SNBaseDataType, SNMask, SNMaskExample
 
-Description = iProperties.Value("Project", "Description")
-PartType = iProperties.Value("Custom", "PartType")
+    Description = iProperties.Value("Project", "Description")
+    PartType = iProperties.Value("Custom", "PartType")
 
-'UOM is set based on part type
-'NOTE: default is "M" (manufactured), though only "P"/"M" are expected
-If StrComp(PartType, "P") = 0 Then
-    UOM = "EAP"
-Else
-    UOM = "EAM"
-End If
+    'UOM is set based on part type
+    'NOTE: default is "M" (manufactured), though only "P"/"M" are expected
+    If StrComp(PartType, "P") = 0 Then
+        UOM = "EAP"
+    Else
+        UOM = "EAM"
+    End If
 
-TrackSerialNum = iProperties.Value("Custom", "TrackSerialNum")
-'if serial number is being tracked, a bunch of fields are enabled
-If TrackSerialNum Then
-    SNFormat = "NF#######"
-    SNBaseDataType = "MASK"
-    SNMask = "NF"
-    SNMaskExample = "NF9999999"
-Else
-    SNFormat = ""
-    SNBaseDataType = ""
-    SNMask = ""
-    SNMaskExample = ""
-End If
+    TrackSerialNum = iProperties.Value("Custom", "TrackSerialNum")
+    'if serial number is being tracked, a bunch of fields are enabled
+    If TrackSerialNum Then
+        SNFormat = "NF#######"
+        SNBaseDataType = "MASK"
+        SNMask = "NF"
+        SNMaskExample = "NF9999999"
+    Else
+        SNFormat = ""
+        SNBaseDataType = ""
+        SNMask = ""
+        SNMaskExample = ""
+    End If
 
-'Open the CSV file (note: this will overwrite the file if it exists!)
-fso = CreateObject("Scripting.FileSystemObject")
-FileName = "I:\Cadd\_iLogic\Export\Part_Level.csv"
-csv = fso.OpenTextFile(FileName, 2, True, -2)
+    'Open the CSV file (note: this will overwrite the file if it exists!)
+    fso = CreateObject("Scripting.FileSystemObject")
+    FileName = "I:\Cadd\_iLogic\Export\Part_Level.csv"
+    csv = fso.OpenTextFile(FileName, 2, True, -2)
 
-Fields = "Company,PartNum,SearchWord,PartDescription,ClassID,IUM,PUM,TypeCode,PricePerCode,ProdCode,MfgComment,PurComment,TrackSerialNum,SalesUM,UsePartRev,SNFormat,SNBaseDataType,UOMClassID,SNMask,SNMaskExample,NetWeightUOM"
+    Fields = "Company,PartNum,SearchWord,PartDescription,ClassID,IUM,PUM,TypeCode,PricePerCode,ProdCode,MfgComment,PurComment,TrackSerialNum,SalesUM,UsePartRev,SNFormat,SNBaseDataType,UOMClassID,SNMask,SNMaskExample,NetWeightUOM"
 
-'Build string containing values in order expected by DMT (see Fields string)
-Data = "BBN"                                'Company name (constant)
-Data = Data & "," & iProperties.Value("Project", "Part Number")
+    'Build string containing values in order expected by DMT (see Fields string)
+    Data = "BBN"                                'Company name (constant)
+    Data = Data & "," & iProperties.Value("Project", "Part Number")
 
-Data = Data & "," & Left(Description, 8)    'Search word, first 8 characters of description
-Data = Data & "," & Description
+    Data = Data & "," & Left(Description, 8)    'Search word, first 8 characters of description
+    Data = Data & "," & Description
 
-Data = Data & "," & iProperties.Value("Custom", "ClassID")
+    Data = Data & "," & iProperties.Value("Custom", "ClassID")
 
-Data = Data & "," & UOM
-Data = Data & "," & UOM
+    Data = Data & "," & UOM
+    Data = Data & "," & UOM
 
-Data = Data & "," & PartType
+    Data = Data & "," & PartType
 
-'Price per grouping (currently: "E", but will this always be the case?)
-Data = Data & "," & "E"
+    'Price per grouping (currently: "E", but will this always be the case?)
+    Data = Data & "," & "E"
 
-Data = Data & "," & iProperties.Value("Custom", "ProdCode")
-Data = Data & "," & iProperties.Value("Custom", "MfgComment")
-Data = Data & "," & iProperties.Value("Custom", "PurComment")
-Data = Data & "," & TrackSerialNum
-Data = Data & "," & UOM
-Data = Data & "," & iProperties.Value("Custom", "UsePartRev")
+    Data = Data & "," & iProperties.Value("Custom", "ProdCode")
+    Data = Data & "," & iProperties.Value("Custom", "MfgComment")
+    Data = Data & "," & iProperties.Value("Custom", "PurComment")
+    Data = Data & "," & TrackSerialNum
+    Data = Data & "," & UOM
+    Data = Data & "," & iProperties.Value("Custom", "UsePartRev")
 
-Data = Data & "," & SNFormat
-Data = Data & "," & SNBaseDataType
+    Data = Data & "," & SNFormat
+    Data = Data & "," & SNBaseDataType
 
-'UOMClassID
-Data = Data & "," & "BBN"
+    'UOMClassID
+    Data = Data & "," & "BBN"
 
-Data = Data & "," & SNMask
-Data = Data & "," & SNMaskExample
+    Data = Data & "," & SNMask
+    Data = Data & "," & SNMaskExample
 
-'Net Weight UOM: only needed for manufactured parts
-If StrComp(PartType, "M") = 0 Then
-    Data = Data & "," & "LB"
-Else
-    Data = Data & ","
-End If
+    'Net Weight UOM: only needed for manufactured parts
+    If StrComp(PartType, "M") = 0 Then
+        Data = Data & "," & "LB"
+    Else
+        Data = Data & ","
+    End If
 
-'Write field headers & data to file
-csv.WriteLine(Fields)
-csv.WriteLine(Data)
-csv.Close()
+    'Write field headers & data to file
+    csv.WriteLine(Fields)
+    csv.WriteLine(Data)
+    csv.Close()
 
-Dim resultmsg As String = exec_DMT("Part", FileName)
-MessageBox(resultmsg)
+    Dim resultmsg As String = exec_DMT("Part", FileName)
+    MsgBox(resultmsg)
+End Sub
