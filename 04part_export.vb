e7a05c44d0a7f021571446859301ee53ae8f4994
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-19 16:10:57 -0400

    Start shifting from CamelCase
    It will still be used for VB-specific constructs and variables that are
    named directly after Epicor fields

--- 04part_export.vb
+++ 04part_export.vb
@@ -1,15 +1,16 @@
 ﻿AddVbFile "dmt.vb"
 
 Sub Main()
+    Dim csv_path As String = "I:\Cadd\_iLogic\Export\"
     Dim dmt_log As String = ""
 
-    dmt_log = dmt_log & Part()
+    dmt_log = dmt_log & Part(csv_path) & Environment.NewLine
     MsgBox(dmt_log)
 End Sub
 
-Function Part()
-    Dim fso, FileName, csv
-    Dim Fields, Data
+Function Part(csv_path As String)
+    Dim fso, file_name, csv
+    Dim fields, data
     Dim Description, PartType, UOM, TrackSerialNum
     Dim SNFormat, SNBaseDataType, SNMask, SNMaskExample
 
@@ -40,56 +41,77 @@ Function Part()
 
     'Open the CSV file (note: this will overwrite the file if it exists!)
     fso = CreateObject("Scripting.FileSystemObject")
-    FileName = "I:\Cadd\_iLogic\Export\Part_Level.csv"
-    csv = fso.OpenTextFile(FileName, 2, True, -2)
+    file_name = csv_path & "Part_Level.csv"
+    csv = fso.OpenTextFile(file_name, 2, True, -2)
 
-    Fields = "Company,PartNum,SearchWord,PartDescription,ClassID,IUM,PUM,TypeCode,PricePerCode,ProdCode,MfgComment,PurComment,TrackSerialNum,SalesUM,UsePartRev,SNFormat,SNBaseDataType,UOMClassID,SNMask,SNMaskExample,NetWeightUOM"
+    fields = "Company,PartNum,SearchWord,PartDescription,ClassID,IUM,PUM,TypeCode,PricePerCode,ProdCode,MfgComment,PurComment,TrackSerialNum,SalesUM,UsePartRev,SNFormat,SNBaseDataType,UOMClassID,SNMask,SNMaskExample,NetWeightUOM"
 
-    'Build string containing values in order expected by DMT (see Fields string)
-    Data = "BBN"                                'Company name (constant)
-    Data = Data & "," & iProperties.Value("Project", "Part Number")
+    'Build string containing values in order expected by DMT (see fields string)
+    data = "BBN"                                'Company name (constant)
+    data = data & "," & iProperties.Value("Project", "Part Number")
 
-    Data = Data & "," & Left(Description, 8)    'Search word, first 8 characters of description
-    Data = Data & "," & Description
+    data = data & "," & Left(Description, 8)    'Search word, first 8 characters of description
+    data = data & "," & Description
 
-    Data = Data & "," & iProperties.Value("Custom", "ClassID")
+    data = data & "," & iProperties.Value("Custom", "ClassID")
 
-    Data = Data & "," & UOM
-    Data = Data & "," & UOM
+    data = data & "," & UOM
+    data = data & "," & UOM
 
-    Data = Data & "," & PartType
+    data = data & "," & PartType
 
     'Price per grouping (currently: "E", but will this always be the case?)
-    Data = Data & "," & "E"
+    data = data & "," & "E"
 
-    Data = Data & "," & iProperties.Value("Custom", "ProdCode")
-    Data = Data & "," & iProperties.Value("Custom", "MfgComment")
-    Data = Data & "," & iProperties.Value("Custom", "PurComment")
-    Data = Data & "," & TrackSerialNum
-    Data = Data & "," & UOM
-    Data = Data & "," & iProperties.Value("Custom", "UsePartRev")
+    data = data & "," & iProperties.Value("Custom", "ProdCode")
+    data = data & "," & iProperties.Value("Custom", "MfgComment")
+    data = data & "," & iProperties.Value("Custom", "PurComment")
+    data = data & "," & TrackSerialNum
+    data = data & "," & UOM
+    data = data & "," & iProperties.Value("Custom", "UsePartRev")
 
-    Data = Data & "," & SNFormat
-    Data = Data & "," & SNBaseDataType
+    data = data & "," & SNFormat
+    data = data & "," & SNBaseDataType
 
     'UOMClassID
-    Data = Data & "," & "BBN"
+    data = data & "," & "BBN"
 
-    Data = Data & "," & SNMask
-    Data = Data & "," & SNMaskExample
+    data = data & "," & SNMask
+    data = data & "," & SNMaskExample
 
     'Net Weight UOM: only needed for manufactured parts
     If StrComp(PartType, "M") = 0 Then
-        Data = Data & "," & "LB"
+        data = data & "," & "LB"
     Else
-        Data = Data & ","
+        data = data & ","
     End If
 
     'Write field headers & data to file
-    csv.WriteLine(Fields)
-    csv.WriteLine(Data)
+    csv.WriteLine(fields)
+    csv.WriteLine(data)
     csv.Close()
 
-    Dim resultmsg As String = DMT.exec_DMT("Part", FileName)
+    Dim resultmsg As String = DMT.exec_DMT("Part", file_name)
     Return resultmsg
+End Function
+
+Function Part_Rev(csv_path As String)
+    Dim fso, file_name, csv
+    Dim fields, data
+    Dim rev_number
+
+    'Open the CSV file (note: this will overwrite the file if it exists!)
+    fso = CreateObject("Scripting.FileSystemObject")
+    file_name = csv_path & "Part_Rev.csv"
+    csv = fso.OpenTextFile(file_name, 2, True, -2)
+
+    fields = "Company,PartNum,RevisionNum,RevShortDesc,RevDescription,Approved,ApprovedDate,ApprovedBy,EffectiveDate,DrawNum,Plant,ProcessMode"
+
+    data = "BBN"                        'Company name (constant)
+    data = data & "," & iProperties.Value("Project", "Part Number")
+    data = data & "," & iProperties.Value("Project", "Revision Number")
+    data = data & "," & "Revision " &               'TODO: RevShortDesc
+    data = data & "," & ""
+
+    Dim resultmsg As String = DMT.exec_DMT("Part Revision", file_name)
 End Function
