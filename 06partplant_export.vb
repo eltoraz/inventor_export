9af90cc51433e0608e4cab93fc67219b796f7c89
Bill Jameson <williamjameson@alum.rpi.edu>
2016-05-21 18:43:00 -0400

    Fill out some more of the logic for Part Plant
    A couple field definitions

--- 06partplant_export.vb
+++ 06partplant_export.vb
@@ -3,14 +3,37 @@ AddVbFile "dmt.vb"
 Sub Main()
     Dim fields, data As String
     Dim PartType As String
-    Dim LeadTime, VendorNum, PurPoint As String     'only valid for purchased parts
+
+    'fields for purchased parts
+    Dim LeadTime, VendorNum As Integer
+    Dim PurPoint As String
+
+    'fields for manufactured parts
+    Dim TrackSerialNumber As Boolean
+    Dim SNMask, SNMaskExample, SNBaseDataType, SNFormat As String
 
     PartType = iProperties.Value("Custom", "PartType")
+    TrackSerialNum = iProperties.Value("Custom", "TrackSerialNum")
 
     If StrComp(PartType, "P") = 0 Then
         'TODO: setup parameters/iProperties for LeadTime et al.
+        'TODO: come to think of it, will purchased parts go through Inventor at all?
     Else
-        'TODO: setup serial number for manufactured parts
+        LeadTime = 0
+        VendorNum = 0
+        PurPoint = ""
+    End If
+
+    If TrackSerialNumber And StrComp(PartType, "M") = 0 Then
+        SNMask = "NF"
+        SNMaskExample = "NF9999999"
+        SNBaseDataType = "MASK"
+        SNFormat = "NF#######"
+    Else
+        SNMask = ""
+        SNMaskExample = ""
+        SNBaseDataType = ""
+        SNFormat = ""
     End If
 
     fields = "Company,Plant,PartNum,PrimWhse,LeadTime,VendorNum,PurPoint,SourceType,CostMethod,SNMask,SNMaskExample,SNBaseDataType,SNFormat"
@@ -28,5 +51,14 @@ Sub Main()
 
     data = data & "," & "F"                         'CostMethod (constant)
 
-    'TODO: Serial Number
+    data = data & "," & SNMask
+    data = data & "," & SNMaskExample
+    data = data & "," & SNBaseDataType
+    data = data & "," & SNFormat
+
+    Dim file_name As String
+    file_name = DMT.write_csv("Part_Plant.csv", fields, data)
+
+    'TODO: verify this is the correct table name in DMT
+    DMT.exec_DMT("Part Plant", file_name)
 End Sub
