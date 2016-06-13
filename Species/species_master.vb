35d97719162ca29a81469379516a835c479bf027
Bill Jameson <williamjameson@alum.rpi.edu>
2016-06-13 16:25:34 -0400

    Make forms modal to prevent second from launching prematurely

--- Species/species_master.vb
+++ Species/species_master.vb
@@ -1,5 +1,5 @@
 'call the rules/open the forms in order to setup the iProperties properly
 iLogicVb.RunExternalRule("10species_parameters.vb")
-iLogicForm.ShowGlobal("20species_select")
-iLogicForm.ShowGlobal("30species_partnum")
+iLogicForm.ShowGlobal("20species_select", FormMode.Modal)
+iLogicForm.ShowGlobal("30species_partnum", FormMode.Modal)
 iLogicVb.RunExternalRule("40species_iproperties.vb")
