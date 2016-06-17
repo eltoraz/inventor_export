3da3bd87abea6b9a329df9fb90208a1537f54fc3
Bill Jameson <williamjameson@alum.rpi.edu>
2016-06-17 16:05:39 -0400

    Start working on species part number validation
    Very simple: the part numbers need to be in the format XX-###

---
+++ Species/40species_validation.vb
@@ -0,0 +1,21 @@
+'validate the parameters for enabled species, and relaunch the form if necessary
+
+Sub Main()
+    Dim inv_doc As Document = ThisApplication.ActiveEditDocument
+    Dim part_doc As PartDocument
+    Dim assm_doc As AssemblyDocument
+    Dim inv_params As UserParameters
+
+    If TypeOf inv_doc Is PartDocument Then
+        part_doc = app.ActiveEditDocument
+        inv_params = part_doc.ComponentDefinition.Parameters.UserParameters
+    ElseIf TypeOf inv_doc Is AssemblyDocument Then
+        assm_doc = app.ActiveEditDocument
+        inv_params = assm_doc.ComponentDefinition.Parameters.UserParameters
+    Else
+        inv_params = inv_doc.ComponentDefinition.Parameters.UserParameters
+    End If
+
+    Dim needs_reentry As NewArrayList()
+    For i = 1 To inv_params.Count
+End Sub
