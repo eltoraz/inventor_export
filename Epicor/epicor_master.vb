2d7823539340ec3dea675a40c71320e25fb0b59e
Bill Jameson <williamjameson@alum.rpi.edu>
2016-06-22 23:06:04 -0400

    Enclose master rules in Sub Main()
    Inventor automatically does this when compiling, but this is for
    consistency in coding style with the other rules

--- Epicor/epicor_master.vb
+++ Epicor/epicor_master.vb
@@ -5,75 +5,77 @@ AddVbFile "60partplant_export.vb"       'PartPlantExport.part_plant_export
 AddVbFile "species_list.vb"             'Species.species_list
 AddVbFile "inventor_common.vb"          'InventorOps.get_param_set
 
-'Pull latest data from Epicor
-'this data shouldn't change often, so the rule shouldn't need to be called often
-'DMT.dmt_export()
+Sub Main()
+    'Pull latest data from Epicor
+    'this data shouldn't change often, so the rule shouldn't need to be called often
+    'DMT.dmt_export()
 
-'Call the other rules in order
-iLogicVb.RunExternalRule("10multi_value.vb")
-iLogicForm.ShowGlobal("20part_properties", FormMode.Modal)
-'iLogicVb.RunExternalRule("21logic_check.vb")
-iLogicVb.RunExternalRule("30set_props.vb")
+    'Call the other rules in order
+    iLogicVb.RunExternalRule("10multi_value.vb")
+    iLogicForm.ShowGlobal("20part_properties", FormMode.Modal)
+    'iLogicVb.RunExternalRule("21logic_check.vb")
+    iLogicVb.RunExternalRule("30set_props.vb")
 
-'populate the PartNumberToUse param multi-value with the activated part numbers
-Dim app As Inventor.Application = ThisApplication
-Dim inv_params As UserParameters = InventorOps.get_param_set(app)
+    'populate the PartNumberToUse param multi-value with the activated part numbers
+    Dim app As Inventor.Application = ThisApplication
+    Dim inv_params As UserParameters = InventorOps.get_param_set(app)
 
-Dim active_parts As New ArrayList()
-For Each s As String in Species.species_list
-    Dim subst As String = Replace(s, "-", "4")
+    Dim active_parts As New ArrayList()
+    For Each s As String in Species.species_list
+        Dim subst As String = Replace(s, "-", "4")
 
-    Dim flag_param As Parameter = inv_params.Item("Flag" & subst)
-    Dim flag_value = flag_param.Value
+        Dim flag_param As Parameter = inv_params.Item("Flag" & subst)
+        Dim flag_value = flag_param.Value
 
-    If flag_value Then
-        'add active parts and materials to the list to present to the user
-        Dim part_param As Parameter = inv_params.Item("Part" & subst)
-        Dim part_value As String = part_param.Value
-        active_parts.Add(part_value)
+        If flag_value Then
+            'add active parts and materials to the list to present to the user
+            Dim part_param As Parameter = inv_params.Item("Part" & subst)
+            Dim part_value As String = part_param.Value
+            active_parts.Add(part_value)
 
-        If StrComp(s, "Hardware") <> 0 Then
-            Dim mat_param As Parameter = inv_params.Item("Mat" & subst)
-            Dim mat_value As String = mat_param.Value
-            active_parts.Add(mat_value)
+            If StrComp(s, "Hardware") <> 0 Then
+                Dim mat_param As Parameter = inv_params.Item("Mat" & subst)
+                Dim mat_value As String = mat_param.Value
+                active_parts.Add(mat_value)
+            End If
         End If
+    Next
+
+    'can't proceed if there isn't a part number for at least one species
+    If active_parts.Count = 0 Then
+        MsgBox("Warning: there are no species defined for this part. Please run " & _
+            "the BBN Species Setup first.")
+        Return
     End If
-Next
+    MultiValue.List("PartNumberToUse") = active_parts
 
-'can't proceed if there isn't a part number for at least one species
-If active_parts.Count = 0 Then
-    MsgBox("Warning: there are no species defined for this part. Please run " & _
-           "the BBN Species Setup first.")
-    Return
-End If
-MultiValue.List("PartNumberToUse") = active_parts
+    'TODO: allow cancelling form here to abort
+    Dim part_selected As Boolean = False
+    Dim pn As String = ""
+    Do
+        iLogicForm.ShowGlobal("35epicor_partselect", FormMode.Modal)
 
-'TODO: allow cancelling form here to abort
-Dim part_selected As Boolean = False
-Dim pn As String = ""
-Do
-    iLogicForm.ShowGlobal("35epicor_partselect", FormMode.Modal)
+        pn = inv_params.Item("PartNumberToUse").Value
+        If StrComp(pn, "") <> 0 Then
+            part_selected = True
+        Else
+            MsgBox("Please select a part to continue with the Epicor export.")
+            iLogicVb.RunExternalRule("dummy.vb")
+        End If
+    Loop While Not part_selected
 
-    pn = inv_params.Item("PartNumberToUse").Value
-    If StrComp(pn, "") <> 0 Then
-        part_selected = True
+    'if part export fails, abort - this will usually mean the part is already
+    'in the DB and so the straight add operation failed
+    Dim dmt_obj As New DMT()
+    Dim ret_value = PartExport.part_export(ThisApplication, dmt_obj)
+    If ret_value = 0 Then
+        PartRevExport.part_rev_export(ThisApplication, dmt_obj)
+        PartPlantExport.part_plant_export(ThisApplication, dmt_obj)
+    ElseIf ret_value = -1 Then
+        MsgBox("Error: DMT timed out. Aborting...")
     Else
-        MsgBox("Please select a part to continue with the Epicor export.")
-        iLogicVb.RunExternalRule("dummy.vb")
+        MsgBox("Warning: this part is already present in Epicor. Aborting...")
     End If
-Loop While Not part_selected
 
-'if part export fails, abort - this will usually mean the part is already
-'in the DB and so the straight add operation failed
-Dim dmt_obj As New DMT()
-Dim ret_value = PartExport.part_export(ThisApplication, dmt_obj)
-If ret_value = 0 Then
-    PartRevExport.part_rev_export(ThisApplication, dmt_obj)
-    PartPlantExport.part_plant_export(ThisApplication, dmt_obj)
-ElseIf ret_value = -1 Then
-    MsgBox("Error: DMT timed out. Aborting...")
-Else
-    MsgBox("Warning: this part is already present in Epicor. Aborting...")
-End If
-
-'TODO: display message box about DMT state - maybe last 3 lines of logfile
+    'TODO: display message box about DMT state - maybe last 3 lines of logfile
+End Sub
--- Species/species_master.vb
+++ Species/species_master.vb
@@ -1,6 +1,8 @@
-'call the rules/open the forms in order to setup the iProperties properly
-iLogicVb.RunExternalRule("10species_parameters.vb")
-iLogicForm.ShowGlobal("20species_select", FormMode.Modal)
-iLogicForm.ShowGlobal("30species_partnum", FormMode.Modal)
-iLogicVb.RunExternalRule("40species_validation.vb")
-iLogicVb.RunExternalRule("50species_iproperties.vb")
+Sub Main()
+    'call the rules/open the forms in order to setup the iProperties properly
+    iLogicVb.RunExternalRule("10species_parameters.vb")
+    iLogicForm.ShowGlobal("20species_select", FormMode.Modal)
+    iLogicForm.ShowGlobal("30species_partnum", FormMode.Modal)
+    iLogicVb.RunExternalRule("40species_validation.vb")
+    iLogicVb.RunExternalRule("50species_iproperties.vb")
+End Sub
