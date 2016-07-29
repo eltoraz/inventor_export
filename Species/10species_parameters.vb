28b4787e69833039d3683ce9f462d1a94d3fc7b6
Bill Jameson <williamjameson@alum.rpi.edu>
2016-07-29 10:51:56 -0400

    Make sure shared params are created in species mod
    Yesterday's tweaks use the new shared parameter `IntermediatePart`,
    which otherwise wouldn't be created in species setup

--- Species/10species_parameters.vb
+++ Species/10species_parameters.vb
@@ -1,10 +1,17 @@
 AddVbFile "inventor_common.vb"      'InventorOps.create_param
 AddVbFile "species_list.vb"         'Species.species_list
+AddVbFile "parameters.vb"           'ParameterLists.shared_params
 
 'create 3 parameters per supported species: one to flag whether it's in use,
 '1 for the part, and 1 for the raw material
 Sub Main()
     Dim inv_app As Inventor.Application = ThisApplication
+
+    'make sure shared parameters are created no matter which module runs first
+    For Each kvp As KeyValuePair(Of String, UnitsTypeEnum) in ParameterLists.shared_params
+        InventorOps.create_param(kvp.Key, kvp.Value, inv_app)
+    Next
+
     For Each s As String in Species.species_list
         'note: Inventor parameters don't support spaces or special characters, so
         'need to do a character substitution on the `-`, then switch back when
