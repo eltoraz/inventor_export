f66a7de8a56cd7ab4d74d26358df1b09c3fffe01
Bill Jameson <williamjameson@alum.rpi.edu>
2016-07-29 16:34:25 -0400

    Start work on quoting spreadsheet file picker

--- Quoting/quoting_master.vb
+++ Quoting/quoting_master.vb
@@ -1,6 +1,9 @@
 AddVbFile "inventor_common.vb"      'InventorOps.get_param_set
 AddVbFile "species_list.vb"         'Species.species_list
 AddVbFile "species_common.vb"       'SpeciesOps.select_active_part
+AddVbFile "quoting_common.vb"       'QuotingOps.starting_path
+
+Imports System.Windows.Forms
 
 Sub Main()
     Dim app As Inventor.Application = ThisApplication
@@ -17,6 +20,15 @@ Sub Main()
     If form_result = FormResult.Cancel OrElse form_result = FormResult.None Then
         Return
     End If
+
+    'open the quoting spreadsheet
+    'using VB-native dialog instead of Inventor since navigating network drives is easier
+    Dim file_picker As New OpenFileDialog()
+    file_picker.InitialDirectory = QuotingOps.starting_path
+    file_picker.Title = "Select Quoting spreadsheet to use..."
+    file_picker.Filter = "Microsoft Excel Spreadsheets (*.xls)|*.xls|All Files (*.*)|*.*"
+    file_picker.FilterIndex = 1
+    'TODO: display dialog & operate on returned filename/path
 
     form_result = iLogicForm.ShowGlobal("quoting_20field_entry", FormMode.Modal).Result
     If form_result = FormResult.Cancel OrElse form_result = FormResult.None Then
