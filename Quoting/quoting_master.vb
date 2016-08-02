21821f24dffb9e11fd81ede18a79d643b62d5811
Bill Jameson <williamjameson@alum.rpi.edu>
2016-08-02 14:24:16 -0400

    Add validation to quoting
    Validate required fields
    TODO: verify ranges (eg, for width/length)

--- Quoting/quoting_master.vb
+++ Quoting/quoting_master.vb
@@ -4,9 +4,10 @@ AddVbFile "species_common.vb"       'SpeciesOps.select_active_part
 AddVbFile "quoting_common.vb"       'QuotingOps.starting_path
 
 Imports System.Windows.Forms
+Imports Inventor
 
 Sub Main()
-    Dim app As Inventor.Application = ThisApplication
+    Dim app As Application = ThisApplication
     Dim inv_params As UserParameters = InventorOps.get_param_set(app)   
 
     Dim form_result As FormResult = FormResult.OK
@@ -36,8 +37,77 @@ Sub Main()
     iLogicVb.RunExternalRule("30quoting_writespreadsheet.vb")
 End Sub
 
-Function validate_quoting(ByRef app As Inventor.Application) As FormResult
-    'TODO: pop up a form to hand-enter value for "Molded" if "Custom" selected
+'validate quoting form data, prompting for reentry of required fields
+Function validate_quoting(ByRef app As Application) As FormResult
+    Dim inv_params As UserParameters = InventorOps.get_param_set(app)
+
+    Dim fails_validation As Boolean = False
+    Dim required_text_fields As New Dictionary(Of String, String) From _
+            {{"QuotingSpreadsheet", "Quoting Spreadsheet"}, {"WidthSpec", "Width Spec"}, _
+             {"LengthSpec", "Length Spec"}, {"SandingSpec", "Sanding Spec"}, _
+             {"GrainDirection", "Grain Direction"}, {"CertifiedClass", "Certified Classification"}, _
+             {"GlueUpSpec", "Glue up or solid stock"}, {"GradeSpec", "Grade Spec"}, _
+             {"Molded", "Molded"}}
+    Dim required_num_fields As New Dictionary(Of String, String) From _
+            {{"FinshedThickness", "Finished Thickness"}, {"Width", "Width"}, _
+             {"Length", "Length"}, {"QtyPerUnit", "Qty Per Unit"}, _
+             {"NestedQty", "Nested Qty"}}
+
     Dim form_result As FormResult = FormResult.OK
+
+    'validation loop - most values are selected from multi-value lists, and the
+    ' default values tend to be "" or 0.0
+    Do
+        Dim error_log = ""
+
+        'pop up a form to hand-enter value for "Molded" if "Custom" selected
+        Dim molded_param As Parameter = inv_params.Item("Molded")
+        If String.Equals(molded_param.Value, "Custom") Then
+            Dim molded_val As String = InputBox("Enter custom molding specification." & _
+                            System.Environment.NewLine & _
+                            "(Clear the input box to abort spreadsheet generation.)", _
+                            "Custom molding specification")
+            If String.IsNullOrEmpty(molded_val) Then
+                form_result = FormResult.Cancel
+                Exit Do
+            Else
+                molded_param.Value = molded_val
+            End If
+        End If
+
+        'check required text parameters
+        For Each kvp As KeyValuePair(Of String, String) In required_text_fields
+            If String.IsNullOrEmpty(inv_params.Item(kvp.Key).Value) Then
+                error_log = error_log & System.Environment.NewLine & _
+                            "- Set a value for " & kvp.Value
+                fails_validation = True
+            End If
+        Next
+
+        'check required numeric parameters
+        For Each kvp As KeyValuePair(Of String, String) In required_num_fields
+            If inv_params.Item(kvp.Key).Value <= 0.0 Then
+                error_log = error_log & System.Environment.NewLine & _
+                            "- Set a value for " & kvp.Value
+                fails_validation = True
+            End If
+        Next
+
+        'set the flag to end the loop if validation passed on this iteration
+        If String.IsNullOrEmpty(error_log) Then
+            fails_validation = False
+        End If
+
+        If fails_validation Then
+            MsgBox("Please correct the problems in the following fields: " & error_log)
+            form_result = iLogicForm.ShowGlobal("quoting_20field_entry", FormMode.Modal).Result
+
+            'abort if the user cancels the form
+            If form_result = FormResult.Cancel OrElse form_result = FormResult.None Then
+                Exit Do
+            End If
+        End If
+    Loop While fails_validation
+
     Return form_result
 End Function
