8f2d4931e9e7961ff737abef9286a48da369b161
Bill Jameson <williamjameson@alum.rpi.edu>
2016-08-03 15:11:44 -0400

    Select a blank row in spreadsheet if mat not present
    If the material is already in the spreadsheet, give the use a choice
    whether to continue; otherwise, find the next blank row (defined by the
    contents of column A, the part number)

--- Quoting/30quoting_writespreadsheet.vb
+++ Quoting/30quoting_writespreadsheet.vb
@@ -16,18 +16,48 @@ Sub Main()
     End Try
 
     'find an open row, or this part's existing entry
-    'TODO: need to account for spreadsheet schema, since GoExcel.FindRow stops
-    '       searching on the first empty cell in the column
+    'NOTE: the spreadsheets this is designed for have no blank rows in between data items:
+    '      starting at row 4, it's all data, so a blank row indicates a free spot for new entries
+    '      (also, GoExcel.FindRow stops searching at the first empty cell in one of its query columns)
+    Dim data_start_row As Integer = 4
+    Dim start_search As Integer = data_start_row
+    Dim max_search As Integer = data_start_row + 100
     Dim working_row As Integer
+
+    'Yes/No message box returns 6 (Yes) or 7 (No) (X/Cancel is disabled)
+    Dim msg_result As Integer = 7
+
+    'first see if the part already has an entry in the spreadsheet
     GoExcel.TitleRow = 2
-    GoExcel.FindRowStart = 4
+
+    GoExcel.FindRowStart = data_start_row
     working_row = GoExcel.FindRow(quoting_spreadsheet, QuotingOps.sheet_name, "Stock Name", "=", pn)
 
-    'DEBUG
-    MsgBox(quoting_spreadsheet & "; " & QuotingOps.sheet_name & System.Environment.NewLine & _ 
-            pn & "; " & working_row)
-    If working_row = -1 Then
-        working_row = 17
+    If working_row <> -1 Then
+        'if the part is already in the spreadsheet, give the user the option to continue or abort
+        msg_result = MsgBox("This material already has an entry in the spreadsheet. " & _
+                            "Would you like to continue to update it?", MsgBoxStyle.YesNo)
+        If msg_result = 7 Then Return
+    Else
+        'entry not present, so look for a blank row
+        Do
+            For i = start_search To max_search
+                If String.IsNullOrEmpty(GoExcel.CellValue("A" & count)) Then
+                    working_row = i
+                    Exit For
+                End If
+            Next
+
+            'no blank row found in search range, so prompt user to continue or abort
+            If working_row = -1 Then
+                msg_result = MsgBox("The spreadsheet already has at least " & _
+                                max_search-data_start_row & " entries. Continue?", _
+                                MsgBoxStyle.YesNo)
+                If msg_result = 7 Then Return
+            End If
+            start_search = max_search
+            max_search += 100
+        Loop While working_row = -1
     End If
 
     'write the quoting fields to their respective cells
