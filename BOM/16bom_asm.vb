971eec299cc3bbaedc6772e4b3f3b5d39099a0d7
Bill Jameson <williamjameson@alum.rpi.edu>
2016-08-08 16:59:04 -0400

    Add skeleton of assembly BOM
    Got as far as fetching the set of unique parts in the assembly
    
    Can use `ThisBOM.CalculateQuantity("Model Data", part_list.Item(i))` to
    get the quantity without needing to count manually, but there may be a
    bug in Inventor 2016 that prevents this from working accurately in all
    cases

--- BOM/16bom_asm.vb
+++ BOM/16bom_asm.vb
@@ -1,3 +1,37 @@
 AddVbFile "dmt.vb"                      'DMT
+AddVbFile "inventor_common.vb"          'InventorOps.get_param_set
+AddVbFile "species_common.vb"           'SpeciesOps.select_active_part
 AddVbFile "bom_common.vb"               'BomOps.bom_fields
 
+Sub Main()
+    Dim inv_app As Inventor.Application = ThisApplication
+    Dim inv_doc As AssemblyDocument = inv_app.ActiveDocument
+    Dim inv_params As UserParameters = InventorOps.get_param_set(inv_app)
+
+    'BOM objects
+    Dim comp_def As AssemblyComponentDefinition
+    comp_def = inv_doc.ComponentDefinition
+    Dim part_list As New List(Of String)()      'iLogic doesn't seem to support HashSet
+
+    'get unique part numbers
+    For Each occ in comp_def.Occurrences
+        Dim occ_name As String = occ.Name
+        Dim colon_pos As Integer = occ_name.IndexOf(":")
+        occ_name = occ_name.Substring(0, colon_pos)
+        If Not part_list.Contains(occ_name) Then part_list.Add(occ_name)
+    Next
+
+    Dim summary_props As PropertySet = inv_doc.PropertySets.Item("Inventor Summary Information")
+
+    Dim selected_part As Tuple(Of String, String, String) = _
+            SpeciesOps.unpack_pn(inv_params.Item("PartNumberToUse").Value)
+
+    Dim PartNum, RevisionNum, MtlPartNum As String
+    Dim MtlSeq As Integer = BomOps.MtlSeqStart 
+
+    'get the part number of the associated material
+    Dim part_species As String = selected_part.Item3
+    Dim subst As String = Replace(part_species, "-", "4")
+
+    Dim data As String = ""
+End Sub
